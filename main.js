d20585d40c8d680a0843a17abb26959294808918
ZillMozeez <55386900+ZillMozeez@users.noreply.github.com>
2025-05-08 22:49:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const form = document.getElementById("form");
+const errorMessage = document.getElementById("error");
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+    if(name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+    if(email.value === '' || email.value == null){
+        messages.push('Email is required')
+    }
+    if(message.value === '' || message.value == null){
+        messages.push('Write a message')
+    }
+
+    if(messages.length > 0){
+        e.preventDefault();
+        errorMessage.innerText = messages.join(', ');
+    }
+});
